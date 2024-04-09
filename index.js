549f5c36b6bb9cfe03e51efe90f6077eb726a655
DDanielPaez <danielpaezy@gmail.com>
2024-04-08 23:10:29 -0300

    extra de index

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const app = require('./app'); //Creando un protocolo de transferencia
-const http = require('http');
-const server = http.createServer(app);
+// const app = require('./app'); //Creando un protocolo de transferencia
+// const http = require('http');
+// const server = http.createServer(app);
 
-server.listen(3000, ()=>{
-    console.log('El servidor esta corriendo');
+// server.listen(3000, ()=>{
+//     console.log('El servidor esta corriendo');
+// })
+
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 9000;
+
+app.listen(port, ()=>console.log('server listen on port', port));
+
+app.get('/',(req,res)=>{
+    res.send('Bienvenido al servidor')
 })
